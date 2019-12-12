25177c82ab4544c30e43a5e9dbb684dfa1c4f0e5
1569413294 <1569413294@qq.com>
2019-12-12 15:49:34 +0800

    医院表

---
+++ src/main/java/com/buba/hospital_back/bean/secHospital.java
@@ -0,0 +1,105 @@
+package com.buba.hospital_back.bean;
+
+import java.util.Date;
+
+public class secHospital {
+    private Integer id;
+
+    private String status;
+
+    private Boolean disabled;
+
+    private Date createtime;
+
+    private String hospitalName;
+
+    private String hospitalAddress;
+
+    private Integer hospitalTel;
+
+    private Double longitude;
+
+    private Double latitude;
+
+    private Integer creator;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status == null ? null : status.trim();
+    }
+
+    public Boolean getDisabled() {
+        return disabled;
+    }
+
+    public void setDisabled(Boolean disabled) {
+        this.disabled = disabled;
+    }
+
+    public Date getCreatetime() {
+        return createtime;
+    }
+
+    public void setCreatetime(Date createtime) {
+        this.createtime = createtime;
+    }
+
+    public String getHospitalName() {
+        return hospitalName;
+    }
+
+    public void setHospitalName(String hospitalName) {
+        this.hospitalName = hospitalName == null ? null : hospitalName.trim();
+    }
+
+    public String getHospitalAddress() {
+        return hospitalAddress;
+    }
+
+    public void setHospitalAddress(String hospitalAddress) {
+        this.hospitalAddress = hospitalAddress == null ? null : hospitalAddress.trim();
+    }
+
+    public Integer getHospitalTel() {
+        return hospitalTel;
+    }
+
+    public void setHospitalTel(Integer hospitalTel) {
+        this.hospitalTel = hospitalTel;
+    }
+
+    public Double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(Double longitude) {
+        this.longitude = longitude;
+    }
+
+    public Double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(Double latitude) {
+        this.latitude = latitude;
+    }
+
+    public Integer getCreator() {
+        return creator;
+    }
+
+    public void setCreator(Integer creator) {
+        this.creator = creator;
+    }
+}
