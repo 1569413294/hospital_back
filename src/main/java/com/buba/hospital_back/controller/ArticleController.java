68b70aa9c6786b1860692b900f4e0a1d58c72a04
qianquan <2897068188@qq.com>
2019-12-13 11:00:41 +0800

    邵哥哥

---
+++ src/main/java/com/buba/hospital_back/controller/ArticleController.java
@@ -0,0 +1,10 @@
+package com.buba.hospital_back.controller;
+
+/**
+ * @ClassName ArticleController
+ * @Description TODO
+ * @Created by Administrator on 2019/12/13 10:59
+ * @Version 1.0
+ **/
+public class ArticleController {
+}
