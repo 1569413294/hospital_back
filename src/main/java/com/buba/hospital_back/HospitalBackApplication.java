64e3f89352b81b471f103b762a6d6f5add76acde
qianquan <2897068188@qq.com>
2019-12-13 10:50:22 +0800

    邵哥哥

--- src/main/java/com/buba/hospital_back/HospitalBackApplication.java
+++ src/main/java/com/buba/hospital_back/HospitalBackApplication.java
@@ -3,6 +3,7 @@ package com.buba.hospital_back;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
